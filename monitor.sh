1f0539621c8e88b823c0764e7371c1d354e24a2c
AbraInsight <github@abrainsight.com>
2017-04-30 20:56:52 +0000

    compute engine startup working

--- monitor.sh
+++ monitor.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "This is working..."
 cd /home/ruth_heather_jones/twitter-spx-volatility
-pip install -r ./requirements.txt
 
-until sudo python ./scraper.py; do
+sudo pip install -r ./requirements.txt
+
+until sudo screen -dm bash -c 'sudo python ./scraper.py'; do
     echo "'scraper.py' crashed with exit code $?. Restarting..." >&2
     sleep 1
 done
