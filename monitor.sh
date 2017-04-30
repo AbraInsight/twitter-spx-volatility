29863aa787c7f45bdb18d5242cf94dfa47f1497d
Ruth Abra <github@abrainsight.com>
2017-04-30 16:12:17 +0100

    updated monitor.sh for Compute Engine

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd /home/ruth_heather_jones/twitter-spx-volatility
+pip install -r ./requirements.txt
+
 until sudo python ./scraper.py; do
     echo "'scraper.py' crashed with exit code $?. Restarting..." >&2
     sleep 1
