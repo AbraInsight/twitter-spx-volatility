f9622035a2274d130dfb9bc9a34c30223700f4de
AbraInsight <github@abrainsight.com>
2017-04-30 21:22:39 +0000

    install supervisor

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-echo "This is working..."
 cd /home/ruth_heather_jones/twitter-spx-volatility
+sudo -H pip install -r ./requirements.txt
 
-sudo pip install -r ./requirements.txt
+#until sudo screen -dm bash -c 'sudo python ./scraper.py'; do
+#    echo "'scraper.py' crashed with exit code $?. Restarting..." >&2
+#    sleep 1
+#done
 
-until sudo screen -dm bash -c 'sudo python ./scraper.py'; do
-    echo "'scraper.py' crashed with exit code $?. Restarting..." >&2
+#sudo screen -dm
+bash -c $(cat <<-ENDOF
+until sudo python ./scraper.py; do
+    echo "restart"
     sleep 1
 done
+ENDOF
+)
